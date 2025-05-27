e6dfc5abaa7e7a0da784953dc4d58303eb80436f
Rio Eka Putra <rioeka45@gmail.com>
2025-05-27 16:21:06 +0700

    Fitur Import Siswa(Rev1)

--- resources/views/admin/siswa/index.blade.php
+++ resources/views/admin/siswa/index.blade.php
@@ -35,11 +35,11 @@
                     {{-- Tombol Tambah --}}
                     <div class="mb-4 flex justify-between items-center">
                         <a href="{{ route('admin.siswa.create') }}"
-                        class="inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        class="inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150">
                             <i class="fas fa-plus mr-2"></i> {{ __('Tambah Siswa') }}
                         </a>
                         <a href="{{ route('admin.siswa.showImportForm') }}"
-                        class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
                             <i class="fas fa-file-import mr-2"></i> {{ __('Impor Siswa dari CSV') }}
                         </a>
                     </div>
